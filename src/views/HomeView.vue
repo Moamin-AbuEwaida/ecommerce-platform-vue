2bf9424951352f11d4eaf62f2bde26b23200cd7c
Moamin <moamin.salamah@gmail.com>
2023-02-22 11:48:19 +0100

    pushing a working project without images rendering

---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <Navbar class="index" />
+    <Dashbaord />
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import Dashbaord from "@/components/Dashbaord.vue";
+import Navbar from "../components/Navbar";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "HomeView",
+
+  components: {
+    Navbar,
+    Dashbaord,
+    Footer,
+  },
+};
+</script>
+
+<style scoped>
+.index {
+  z-index: 999;
+}
+</style>
